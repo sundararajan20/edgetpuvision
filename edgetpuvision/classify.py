a7bdcdc6ff66bfbb5b7b4f40ab052cb662794942
Jonas Larsson <ljonas@google.com>
2019-09-27 17:01:07 -0700

    Use classify_with_input_tensor instead of ClassifyWithInputTensor
    ClassifyWithInputTensor is deprecated and prints a warning every inference.
    
    Change-Id: Ibf6b8cba1ceb9592a4716a0d892ce511238e939a

--- edgetpuvision/classify.py
+++ edgetpuvision/classify.py
@@ -126,7 +126,7 @@ def render_gen(args):
         inference_rate = next(fps_counter)
         if draw_overlay:
             start = time.monotonic()
-            results = engine.ClassifyWithInputTensor(tensor, threshold=args.threshold, top_k=args.top_k)
+            results = engine.classify_with_input_tensor(tensor, threshold=args.threshold, top_k=args.top_k)
             inference_time = time.monotonic() - start
 
             results = [(labels[i], score) for i, score in results]
