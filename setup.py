6a557c205cb2935e8be6d56442e5c5f9a78d958e
Dmitry Kovalev <dkovalev@google.com>
2020-11-10 14:19:27 -0800

    Version in setup.py matched debian package
    Change-Id: Ic3f33b6e496e4106ac4b8dda4576488e32823b30

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='edgetpuvision',
-    version='1.0',
+    version='7.0',
     description='EdgeTPU camera API',
     long_description='API to run inference on image data coming from the camera.',
     author='Coral',
